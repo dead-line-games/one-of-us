d9075dc477462e71ed70c750c2b3015e8f7b25e8
Rafael Jacinto <rpdjacinto@gmail.com>
2016-11-06 21:38:08 -0800

    Added Camera Follow and Parallax scripts

---
+++ one-of-us/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float xMin, xMax, yMin, yMax;
+
+	private Transform target;
+
+	void Start () {
+		target = GameObject.Find ("Mega Man").transform;
+	}
+
+	void LateUpdate () {
+		transform.position = new Vector3 (
+			Mathf.Clamp (target.position.x, xMin, xMax),
+			Mathf.Clamp (target.position.y, yMin, yMax),
+			transform.position.z
+		);
+	}
+}
---
+++ one-of-us/Assets/Scripts/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	public Transform[] backgrounds;
+	public float smoothing;
+
+	private float[] parallaxScales;
+	private Vector3 previousCameraPosition;
+
+	void Start () {
+		previousCameraPosition = transform.position;
+		parallaxScales = new float[backgrounds.Length];
+
+		for (int i = 0; i < parallaxScales.Length; i++) {
+			parallaxScales[i] = backgrounds[i].position.z * -1;
+		}
+
+	}
+
+	void LateUpdate () {
+		for (int i = 0; i < backgrounds.Length; i++) {
+			Vector3 parallax = (previousCameraPosition - transform.position) * (parallaxScales[i] / smoothing);
+			backgrounds [i].position = new Vector3 (
+				backgrounds [i].position.x + parallax.x,
+				backgrounds [i].position.y + parallax.y,
+				backgrounds [i].position.z
+			);
+		}
+		previousCameraPosition = transform.position;
+	}
+}
