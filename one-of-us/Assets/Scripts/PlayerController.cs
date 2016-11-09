c3a65fd4fe7f2749e14054c23192efb9c690a537
Ayumi <imaizumi.ayumi@gmail.com>
2016-11-08 19:11:52 -0800

    added playercontroller :)

---
+++ one-of-us/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+
+	Animator animator;
+	Rigidbody2D physics;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+		physics = GetComponent<Rigidbody2D>();
+	}
+		
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+
+		animator.SetFloat ("speed", Mathf.Abs (move));
+
+		physics.velocity = new Vector2(move * maxSpeed, physics.velocity.y);
+
+		if ((move > 0 && !facingRight) || (move < 0 && facingRight)) {
+			Flip ();
+		}
+	}
+
+	void Flip () {
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
