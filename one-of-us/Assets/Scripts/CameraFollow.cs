33e059d1b2f7978a33072273f9ccaa32b14f9887
Rafael Jacinto <rpdjacinto@gmail.com>
2016-11-14 22:17:39 -0800

    Fixed errors and styling

--- one-of-us/Assets/Scripts/CameraFollow.cs
+++ one-of-us/Assets/Scripts/CameraFollow.cs
@@ -18,4 +18,5 @@ public class CameraFollow : MonoBehaviour {
 			transform.position.z
 		);
 	}
+	
 }
--- one-of-us/Assets/Scripts/IUseable.cs
+++ one-of-us/Assets/Scripts/IUseable.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public interface: IUseable {
+public interface IUseable {
 	void Use();
 }
