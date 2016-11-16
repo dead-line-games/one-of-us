ab0f3a6b86e4e6215f345515869c70c0ef0fcbf0
Caroline (Jia Xin) Chang <chang.majusaka@gmail.com>
2016-11-15 18:44:48 -0800

    fix jump

--- one-of-us/Assets/Scripts/playerJump.cs
+++ one-of-us/Assets/Scripts/playerJump.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class playerJump : MonoBehaviour {
 
+	public int jumpHeight;
+	public int maxJump;
+	private int jumpNum;
+
 	// Use this for initialization
 	void Start () {
-	
+		jumpNum = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Space) && (jumpNum<maxJump)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, jumpHeight);
+			jumpNum++;
+		}
 	}
+	void OnCollisionEnter2D (Collision2D call){
+		if (call.gameObject.CompareTag("Ground")){
+			jumpNum = 0;
+		}
+			}
 }
