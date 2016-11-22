a0939fe229a0484c37caabf4c0b1d51c320cea22
Caroline (Jia Xin) Chang <chang.majusaka@gmail.com>
2016-11-21 23:21:59 -0800

    pause menue basic

---
+++ one-of-us/Assets/Scripts/pause menu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pausemenu : MonoBehaviour {
+    bool pause = false;
+    string chapter;
+    string main;
+    GameObject pauseMenuCanvas;
+	// Use this for initialization
+	void Start () {
+        bool pause = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause = togglePause();
+        }
+        if (pause)
+        {
+            pauseMenuCanvas.SetActive(true);
+        }else
+        {
+            pauseMenuCanvas.SetActive(false);
+        }
+	}
+
+    void OnGUI()
+    {
+        if (pause)
+        {
+            GUILayout.Label("Menu");
+            if (GUILayout.Button("resume"))
+            {
+                pause = togglePause();
+            }
+        }
+    }
+    bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
+}
